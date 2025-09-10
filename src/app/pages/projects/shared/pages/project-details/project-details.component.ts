05ec6c2b10973250aebc1dc5c9d97ab9b8d282b3
RaminAbd <rmnabdulaev@gmail.com>
2025-09-10 19:55:24 +0400

    some changes

--- src/app/pages/projects/shared/pages/project-details/project-details.component.ts
+++ src/app/pages/projects/shared/pages/project-details/project-details.component.ts
@@ -5,6 +5,7 @@ import { ProjectDetailsService } from './project-details.service';
 import {ProjectsResponseModel} from '../../../../admin-projects/shared/models/projects-response.model';
 import {TranslatePipe} from '@ngx-translate/core';
 import {GoogleMap, MapMarker} from '@angular/google-maps';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-project-details',
@@ -18,6 +19,24 @@ import {GoogleMap, MapMarker} from '@angular/google-maps';
   ],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss',
+  animations: [
+    trigger('zoomInOut', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)' }),
+        animate('300ms ease-in-out', style({ transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in-out', style({ transform: 'scale(0.5)' })),
+      ]),
+    ]),
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ProjectDetailsComponent {
   private route: ActivatedRoute = inject(ActivatedRoute);
@@ -33,9 +52,20 @@ export class ProjectDetailsComponent {
     lat: 41.7276044,
     lng: 44.6419557,
   };
-
+  selectedImage: string;
+  showImageLightBox: boolean = false;
   constructor() {
     this.service.component = this;
     this.service.getItem()
+  }
+
+  openImage(item: any) {
+    this.selectedImage = item;
+    console.log(item)
+    this.showImageLightBox = true;
+  }
+
+  closeImageLightbox() {
+    this.showImageLightBox = false;
   }
 }
